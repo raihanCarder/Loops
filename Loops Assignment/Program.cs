f71083d8cbe03a6a7904f94c7be0cc68aee88240
Raihan Carder <339509226@GoTVDSB.ca>
2023-09-26 10:11:15 -0400

    Done 3/5

--- Loops Assignment/Program.cs
+++ Loops Assignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,10 @@ namespace Loops_Assignment
     {
         static void Main(string[] args)
         {   // Raihan Carder        
-            Prompter();
+            //Prompter();
+            //PercentPassing();
+            //OddSum();
+            RandomNumbers();
             Console.ReadLine();
         }
 
@@ -70,6 +74,141 @@ namespace Loops_Assignment
             }
             
             Console.WriteLine("You win, Press enter.");
+            Console.ReadLine();
+        }
+
+        public static void PercentPassing() // Still needs work redo so that user enters numbers first than presses something to end the while loop
+        {
+            int testsAmount = 0, aboveSeventy = 0;
+            double testScore;
+            bool validAmount = false;
+
+
+            Console.WriteLine("Figure out your % of Tests above 70%!");
+            Console.WriteLine();
+            Console.Write("How many tests are you going to enter: ");
+            while (validAmount == false)
+            {
+                if (int.TryParse(Console.ReadLine(), out testsAmount) && testsAmount > 0)
+                {
+                    validAmount = true;
+                }
+                else
+                {
+                    Console.Write("ERROR, How many tests are you going to enter: ");
+                }
+            }
+            validAmount = false;
+
+            for (int i = 1; i < testsAmount+1; i++)
+            {
+                Console.Write($"Enter test mark {i}: ");
+                if (double.TryParse(Console.ReadLine(), out testScore) && testScore > 0 && testScore <= 100)
+                {
+                    testsAmount++;
+
+                    if (testScore > 70)
+                    {
+                       aboveSeventy ++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry, input will not be counted.");
+                }
+            }
+            Console.WriteLine(aboveSeventy);
+            Console.WriteLine(testsAmount);
+            Console.WriteLine($"Your total amount of tests above 70% are: {Convert.ToDouble((aboveSeventy/testsAmount) * 100)}%");
+            Console.ReadLine();
+        }
+
+        public static void OddSum()
+        {
+            int input = 0, sum = 0;
+            bool valid = false;
+
+            Console.WriteLine("ODDSUM INTEGERS");
+            Console.WriteLine();
+            Console.Write("Enter an integer above 0: ");
+            while (valid == false)
+            {
+                if (int.TryParse(Console.ReadLine(), out input) && input > 0)
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.Write("ERROR, enter an integer above 0: ");
+                }
+            }
+
+            for (int i = 1; i < input+1; i+=2)
+            {
+                sum += i;
+            }
+            Console.WriteLine() ;
+            Console.WriteLine($"The sum of all odd integers from 1 to the number entered is: {sum}.");
+            Console.ReadLine();
+        }
+
+        public static void RandomNumbers()
+        {
+            int maximum = 0, minimum = 0, randomValue;
+            bool validMinimum = false, validMaximum = false;
+            string punctuation;
+            Random generator = new Random();
+
+
+            Console.WriteLine("Random Integer Numbers");
+            Console.WriteLine();
+            Console.Write("Enter a Minimum value: ");
+            while (validMinimum == false)
+            {
+
+                if (int.TryParse(Console.ReadLine(), out minimum))
+                {
+                    validMinimum = true;
+                }
+                else
+                {
+                    Console.Write("Please enter a valid integer: ");
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Enter a maximum value: ");
+
+            while (validMaximum == false)
+            {
+                if (int.TryParse(Console.ReadLine(), out maximum) && maximum >= minimum)
+                {
+                    validMaximum = true;
+                }
+                else
+                {
+                    Console.Write("Enter a maximum value that's an integer and bigger than the minimum: ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("25 Values within those parameters are: ");
+
+            for (int i = 0; i<25; i++)
+            {
+                if (i < 24)
+                {
+                    punctuation = ", ";
+                }
+                else
+                {
+                    punctuation = ".";
+                }
+                randomValue = generator.Next(minimum, maximum+1);
+
+                Console.Write(randomValue + punctuation);
+            }
+
+
             Console.ReadLine();
         }
     }
